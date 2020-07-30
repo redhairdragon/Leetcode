7f048f2245425ff46989c0d75ab80a0d5614a64c
Cocoa Starrion <shen_teng@engineering.ucla.edu>
2020-07-29 21:37:30 -0700

    update 76

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -10,40 +10,65 @@ class Solution:
                 track[i] = 1
         letter_collected = len(track)
 
-        
+        # print(track)
         left = 0 
         right = 0
 
         best_left = -1
         best_right = -1
-        best_size = len(S)+2
+        best_size = len(s)+2
 
 
         while True:
-            while letter_collected != 0 and right < len(S):
-                if S[right] in track:
-                    track[S[right]] -= 1
-                    if track[S[right]] == 0:
+            while right < len(s):
+                # print("s right: "+ s[right])
+                if s[right] in track: 
+                    track[s[right]] -= 1
+                    if track[s[right]] == 0:
                         letter_collected -= 1
+                        # print(letter_collected)
+                        # print(track)
+                        # print(s[right])
                         if letter_collected == 0:
+                            right += 1
                             break
                 right += 1
+                
+                
+            # print("finish moving right ")
 
-            while letter_collected == 0:
+            while left<right and letter_collected == 0:
+                # print("left moved")
+                # print("s left: "+ s[left])
+
                 if right - left < best_size:
                     best_left = left
                     best_right = right
-                left += 1
-                if S[left] in track:
-                    track[S[left]] += 1
-                    if track[S[right]] == 1:
+                    best_size = right-left
+                    # print("update best")
+                    # print(best_left)
+                    # print(best_right)
+                    # print(s[best_left:best_right])
+
+                if s[left] in track:
+                    track[s[left]] += 1
+                    # print(letter_collected)
+                    # print(track)
+                    if track[s[left]] == 1:
                         letter_collected += 1
-        
+                        left += 1
+                        break;
+                left += 1
+            # print("finish moving left ")
+
+            if right == len(s):
+                break
+            
         if best_left == -1:
             return ""
-        return S[best_left:best_right]
+        return s[best_left:best_right]
 
 
                 
             
-print(Solution().minWindow("ADOBECODEBANC","ABC"))
+print(Solution().minWindow("ADOBECODEBANC","CD"))
